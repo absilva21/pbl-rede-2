b473c86a4403bffee3f15ec91530fda899cc9227
Alisson Bomfim <alissonbomfimsilva@outlook.com>
2023-09-24 14:43:51 -0300

    Recebendo menssagem

--- Zaps/src/Application.java
+++ Zaps/src/Application.java
@@ -1,12 +1,33 @@
 import java.net.*;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.io.*;
 
 
+
 public class Application {
-
+	public static LinkedList<String> menssagens;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	
+		
+		menssagens = new  LinkedList<String>();
+		
+		Receiver receptor = new Receiver();
+		receptor.run();
+	
 
+	}
+	
+	public static void novaMensagem() {
+
+	    Iterator<String> men = menssagens.iterator();
+	    
+	    while(men.hasNext()) {
+	    	String mensagem = men.next();
+	    	System.out.println("\nfulano disse: \n "+mensagem);
+	    }
 	}
 
 }
--- Zaps/src/Receiver.java
+++ Zaps/src/Receiver.java
@@ -1,6 +1,8 @@
+import java.net.*;
+import java.io.*;
 
 public class Receiver extends Thread {
-
+	
 	public Receiver() {
 		// TODO Auto-generated constructor stub
 		
@@ -8,7 +10,30 @@ public class Receiver extends Thread {
 	
 	@Override
 	public void run() {
+		int porta = 7000;
+		DatagramSocket serverSocket;
+		try {
+			serverSocket = new DatagramSocket(porta);
+			byte[] receiveData = new byte[1024];
+			System.out.println("aguardando mensagens...");
+			while(true) {
+				DatagramPacket receivePacket = new DatagramPacket(receiveData,
+						receiveData.length);
+				serverSocket.receive(receivePacket);
+				
+				String mensagem = new String(receivePacket.getData());
+				System.out.println("\nchegou");
+				Application.menssagens.add(mensagem);
+				Application.novaMensagem();
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		
+			
 	}
 	
 }
